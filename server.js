d4bb2b3c324636820abafa7e75ee98eee6bf0330
TarasChornyi <ct402ct@gmail.com>
2016-10-05 13:16:04 +0300

    Language support.

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-var express = require("express");
-var app = express();
-var router = express.Router();
-var path = __dirname + '/Views/';
+var express = require("express"),
+	app = express(),
+	http = require('http'),
+	router = express.Router(),
+	i18n = require('i18n'),
+	path = __dirname + '/Views/';
+
+i18n.configure({
+    locales:['en', 'ru', 'ua'],
+    directory: __dirname + '/locales',
+	defaultLocale: 'ua',
+ 
+    // query parameter to switch locale (ie. /home?lang=ch) - defaults to NULL 
+    queryParameter: 'lang'
+});
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -9,15 +20,18 @@ router.use(function (req,res,next) {
 });
 
 router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+  res.render(path + "index.ejs");
 });
 
-router.get("/license",function(req,res){
-  res.sendFile(path + "license.html");
+router.get("/license.html",function(req,res){
+  res.render(path + "license.ejs");
 });
 
-router.get("/certificate",function(req,res){
-  res.sendFile(path + "certificate.html");
+router.get("/certificate.html",function(req,res){
+  res.render(path + "certificate.ejs");
+});
+router.get("/katalog.html",function(req,res){
+  res.render(path + "katalog.ejs");
 });
 /*
 router.get("/images",function(req,res){
@@ -31,6 +45,7 @@ router.get("/assets",function(req,res){
 });
 */
 app.use("/",router);
+//app.use(i18n);
 app.use("/assets", express.static(__dirname + '/assets'));
 app.use("/images", express.static(__dirname + '/images'));
 
@@ -39,5 +54,5 @@ app.use("*",function(req,res){
 });
 
 app.listen(9000,function(){
-  console.log("Live at Port 3000");
+  console.log("Live at Port 9000");
 });
